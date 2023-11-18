f3b2faffde2fdcc53393498c00a609f46a4f3188
yoonseo232 <ys.bang232@gmail.com>
2023-11-18 22:16:48 +0900

    서버에 넣기

--- src/main/resources/static/js/join.js
+++ src/main/resources/static/js/join.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
         if (validateId(idValue) && validatePassword(pwValue)) {
             if (pwValue === pwCheckValue) {
-                console.log(idValue, pwValue);
+                accountJoin($("#joinForm").serializeObject());
             } else {
                 alert("비밀번호와 비밀번호 확인이 일치하지 않습니다");
             }
@@ -14,14 +14,31 @@ document.addEventListener("DOMContentLoaded", function() {
             alert("아이디와 비밀번호를 조건에 맞게 입력하세요");
         }
     });
-
-    function validateId(id) {
-        let idPattern = /^[A-Za-z0-9]{3,8}$/; //3글자 이상 8글자 이하
-        return idPattern.test(id);
-    }
-
-    function validatePassword(password) {
-        let passwordPattern = /^[A-Za-z0-9!@#$%^&*()_+{}|:"<>?~,./;'[\]\\-]{4,20}$/; //4글자 이상 20글자 이하
-        return passwordPattern.test(password);
-    }
 });
+
+function validateId(id) {
+    let idPattern = /^[A-Za-z0-9]{3,8}$/; //3글자 이상 8글자 이하
+    return idPattern.test(id);
+}
+
+function validatePassword(password) {
+    let passwordPattern = /^[A-Za-z0-9!@#$%^&*()_+{}|:"<>?~,./;'[\]\\-]{4,20}$/; //4글자 이상 20글자 이하
+    return passwordPattern.test(password);
+}
+
+function accountJoin(formData) {
+    $.ajax({
+        type: "POST",
+        url: "/join",
+        data: JSON.stringify(formData),
+        dataType: "json",
+        contentType: "application/json",
+        success: function(result) {
+            console.log(result);
+            // location.href
+        },
+        error: function (result) {
+            alert(result.error);
+        }
+    });
+}
